5b555aa6f2e3ff3bd058a0d10e1479496c4003d4
kishmwangi <kishmwangi31@gmail.com>
2024-04-27 07:59:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TransactionList  from './TransactionList';
+import TransactionForm from './TransactionForm';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-export default App;
+  const  handleSubmit = (FormData) => {
+    const newTransaction = {
+      description: FormData.description,
+      amount: parseFloat(formData.amount)
+    };
+    setTransactions([...transactions, newTransaction]);
+  }
+};
