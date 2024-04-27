9534e2478ba0191eaada660e0b602f1015cc4d54
kishmwangi <kishmwangi31@gmail.com>
2024-04-27 08:16:47 +0300

    new update

---
+++ src/TransactionForm.js
@@ -0,0 +1,12 @@
+import React, { useState } form 'react';
+
+
+function TransactionForm({ onSubmit }) {
+    const [FormData, setFormData] = useState({
+        description: '',
+        amount: ''
+    });
+}
+
+
+export default TransactionForm;
